fda8a2fffcad0b57800b7cbec48efcd01c1cadb5
Daniel Opdahl <dopdahl16@gmail.com>
2020-02-27 07:27:22 -0600

    additions

--- Assignments/CS370Assignment2.rkt
+++ Assignments/CS370Assignment2.rkt
@@ -35,8 +35,7 @@
       ((null? lis+) '())
       ((zero? n) '(...))
       ((null? (cdr lis+)) (cons (car lis+) '()))
-      (else (cons (get-first-n-elements (car lis+) n) (elide-length (cdr lis+) (- n 1))))
-      )))
+      (else (cons (get-first-n-elements (car lis+) n) (elide-length (cdr lis+) (- n 1)))))))
 
 
 
@@ -59,31 +58,46 @@
 (define elide-depth
   (lambda (lis+ n)
     (cond
-      ((zero? n) '&)
-      ((null? lis+) '()))))
+      ((and (zero? n) (null? lis+)) '&)
+      ((zero? n) (cons '& (elide-depth (cdr lis+) n)))
+      ((null? lis+) '())
+      ((null? (cdr lis+)) (car lis+))
+      (else (cons (elide-depth (car lis+) (- n 1)) (elide-depth (cdr lis+) (- n 1)))))))
 
 
-
+(elide-depth '(() (a) ((a)) (((a)))) 1)
 ;
 ;
 ;
 
-(define perms
-  (lambda (loa)
-    (cond
-      )))
-
-
-
+;(define perms
+;  (lambda (loa)
+;    (cond
+;      ((null? loa) '())
+;      ((null? (car loa)) (car loa))
+;      ((null? (cdr (cdr loa))) (cons (cons (car loa) (cdr loa)) (cons (cons (car (cdr loa)) (cons (car loa) '())) '())))
+;      (else (insert-into-every-space (car loa) (perms (cdr loa)))))))
 ;
 ;
 ;
+;(define insert-into-every-space
+;  (lambda (char lol)
+;    (cond
+;      ((null? lol) '()) 
+;      (cons 
+
+
+
 
 (define subst-every-other
   (lambda (old new los+)
+    (subst-every-other-helper old new los+ 0)))
+
+(define subst-every-other-helper
+  (lambda (old new los+ bit)
     (cond
       ((null? los+) '())
-      ((eq? (car los+) old) (cons new (cdr los+)))
-      (else (cons (subst-every-other old new (car los+)) (subst-every-other old new (cdr los+))))
-      )))
-
+      ((not (atom? (car los+))) (cons (subst-every-other-helper old new (car los+) bit) (subst-every-other-helper old new (cdr los+) bit)))
+      ((and (eq? (car los+) old) (not (zero? bit))) (cons old (subst-every-other-helper old new (cdr los+) 0)))
+      ((and (eq? (car los+) old) (zero? bit)) (cons new (subst-every-other-helper old new (cdr los+) 1)))
+      (else (cons (car los+) (subst-every-other-helper old new (cdr los+) bit))))))
